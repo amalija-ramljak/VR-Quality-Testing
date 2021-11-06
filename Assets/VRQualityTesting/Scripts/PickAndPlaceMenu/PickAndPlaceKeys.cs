24cf17a9dcf2aef8801ea83af47d6bdb244cce3d
Amalija <amalija.ramljak@gmail.com>
2021-11-06 12:19:34 +0100

    add tab initialisation and setting saving

--- Assets/VRQualityTesting/Scripts/PickAndPlaceMenu/PickAndPlaceKeys.cs
+++ Assets/VRQualityTesting/Scripts/PickAndPlaceMenu/PickAndPlaceKeys.cs
@@ -4,6 +4,16 @@ namespace VRQualityTesting.Scripts.PickAndPlaceMenu
     {
         private const string Prefix = "PickAndPlace";
 
-        // public const string FieldName = Prefix + "FieldName";
+        public const string MinTranslation = Prefix + "MinTranslation";
+        public const string MaxTranslation = Prefix + "MaxTranslation";
+        public const string MinRotation = Prefix + "MinRotation";
+        public const string MaxRotation = Prefix + "MaxRotation";
+        public const string ObjectCount = Prefix + "ObjectCount";
+
+        public const string UseObjectTypeSquare = Prefix + "UseObjectTypeSquare";
+        public const string useObjectTypeCylinder = Prefix + "useObjectTypeCylinder";
+        public const string UseObjectTypeSphere = Prefix + "UseObjectTypeSphere";
+
+        public const string RoundDuration = Prefix + "RoundDuration";
     }
 }
--- Assets/VRQualityTesting/Scripts/PickAndPlaceMenu/Scribe.cs
+++ Assets/VRQualityTesting/Scripts/PickAndPlaceMenu/Scribe.cs
@@ -8,36 +8,70 @@ namespace VRQualityTesting.Scripts.PickAndPlaceMenu
 {
     public class Scribe : MonoBehaviour
     {
+        // Objects tab
         [SerializeField] private TMP_InputField minTranslationField;
         [SerializeField] private TMP_InputField maxTranslationField;
         [SerializeField] private TMP_InputField minRotationField;
         [SerializeField] private TMP_InputField maxRotationField;
-        [SerializeField] private TMP_InputField objectCount;
-        [SerializeField] private Toggle useObjectTypeSquare;
-        [SerializeField] private Toggle useObjectTypeCylinder;
-        [SerializeField] private Toggle useObjectTypeSphere;
+        [SerializeField] private TMP_InputField objectCountField;
+        [SerializeField] private Toggle useObjectTypeSquareField;
+        [SerializeField] private Toggle useObjectTypeCylinderField;
+        [SerializeField] private Toggle useObjectTypeSphereField;
         // [SerializeField] private TMP_Dropdown weaponTypeDropdown;
+
+        // Round tab
+        [SerializeField] private TMP_InputField roundDurationField;
 
         private void Start() => InitializeSettings();
 
         private void InitializeSettings()
         {
-            InitializeTabSettings();
+            InitializeObjectsSettings();
+            InitializeRoundSettings();
         }
 
-        private void InitializeTabSettings()
+        private void InitializeObjectsSettings()
         {
-            // <fieldName>Field.text = Settings.GetFloat(PickAndPlaceKeys.<FieldName>, defaultValue: <num>f).ToString(CultureInfo.InvariantCulture);
+            minTranslationField.text = Settings.GetFloat(PickAndPlaceKeys.MinTranslation, defaultValue: 0f).ToString(CultureInfo.InvariantCulture);
+            maxTranslationField.text = Settings.GetFloat(PickAndPlaceKeys.MaxTranslation, defaultValue: 0f).ToString(CultureInfo.InvariantCulture);
+            minRotationField.text = Settings.GetFloat(PickAndPlaceKeys.MinRotation, defaultValue: 0f).ToString(CultureInfo.InvariantCulture);
+            maxRotationField.text = Settings.GetFloat(PickAndPlaceKeys.MaxRotation, defaultValue: 0f).ToString(CultureInfo.InvariantCulture);
+
+            objectCountField.text = Settings.GetInt(PickAndPlaceKeys.ObjectCount, defaultValue: 0).ToString(CultureInfo.InvariantCulture);
+
+            useObjectTypeSquareField.isOn = Settings.GetBool(PickAndPlaceKeys.UseObjectTypeSquare, defaultValue: true).ToString(CultureInfo.InvariantCulture);
+            useObjectTypeCylinderField.isOn = Settings.GetBool(PickAndPlaceKeys.UseObjectTypeCylinder, defaultValue: true).ToString(CultureInfo.InvariantCulture);
+            useObjectTypeSphereField.isOn = Settings.GetBool(PickAndPlaceKeys.UseObjectTypeSphere, defaultValue: true).ToString(CultureInfo.InvariantCulture);
+        }
+
+        private void InitializeRoundSettings()
+        {
+            roundDurationField.text = Settings.GetFloat(PickAndPlaceKeys.RoundDuration, defaultValue: 60f).ToString(CultureInfo.InvariantCulture);
         }
 
         public void SaveSettings()
         {
-            SaveTabSettings();
+            SaveObjectsSettings();
+            SaveRoundSettings();
         }
 
-        private void SaveTabSettings()
+        private void SaveObjectsSettings()
         {
-            // Settings.SetFloat(PickAndPlaceKeys.<FieldName>, float.Parse(<fieldName>Field.text, CultureInfo.InvariantCulture));
+            Settings.SetFloat(PickAndPlaceKeys.MinTranslation, float.Parse(minTranslationField.text, CultureInfo.InvariantCulture));
+            Settings.SetFloat(PickAndPlaceKeys.MaxTranslation, float.Parse(maxTranslationField.text, CultureInfo.InvariantCulture));
+            Settings.SetFloat(PickAndPlaceKeys.MinRotation, float.Parse(minRotationField.text, CultureInfo.InvariantCulture));
+            Settings.SetFloat(PickAndPlaceKeys.MaxRotation, float.Parse(maxRotationField.text, CultureInfo.InvariantCulture));
+
+            Settings.SetInt(PickAndPlaceKeys.ObjectCount, int.Parse(objectCountField.text, CultureInfo.InvariantCulture));
+
+            Settings.SetBool(PickAndPlaceKeys.UseObjectTypeSquare, useObjectTypeSquareField.isOn);
+            Settings.SetBool(PickAndPlaceKeys.UseObjectTypeCylinder, useObjectTypeCylinderField.isOn);
+            Settings.SetBool(PickAndPlaceKeys.UseObjectTypeSphere, useObjectTypeSphereField.isOn);
+        }
+
+        private void SaveRoundSettings()
+        {
+            Settings.SetFloat(PickAndPlaceKeys.RoundDuration, float.Parse(roundDurationField.text, CultureInfo.InvariantCulture));
         }
     }
 }
