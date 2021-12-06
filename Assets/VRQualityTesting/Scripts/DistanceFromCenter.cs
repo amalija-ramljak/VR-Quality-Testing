89eb626c9743608d72112a3da00883beda9fad33
BrunosAccount <bruno.bernik@gmail.com>
2021-12-06 19:46:47 +0100

    PrviPutDodana DistanceFromCenter skripta

---
+++ Assets/VRQualityTesting/Scripts/DistanceFromCenter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistanceFromCenter : MonoBehaviour
+{
+    public Transform other;
+    
+    void Update() {
+        
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (other)
+        {
+            float dist = Vector3.Distance(other.position, transform.position);
+            print("Distance to other: " + dist);
+        }
+    }
+
+    void OnCollisionExit(Collision collision) {
+        if (other)
+        {
+            float dist = Vector3.Distance(other.position, transform.position);
+            print("Ćao");
+        }
+    }
+}
