f2c639346a2bb89d708effe1358b089c0dcf9cc1
Amalija <amalija.ramljak@gmail.com>
2021-12-13 20:43:59 +0100

    write out new parameters and add them to the menu

--- Assets/VRQualityTesting/Scripts/PickAndPlaceMenu/PickAndPlaceKeys.cs
+++ Assets/VRQualityTesting/Scripts/PickAndPlaceMenu/PickAndPlaceKeys.cs
@@ -4,16 +4,28 @@ namespace VRQualityTesting.Scripts.PickAndPlaceMenu
     {
         private const string Prefix = "PickAndPlace";
 
-        public const string MinTranslation = Prefix + "MinTranslation";
-        public const string MaxTranslation = Prefix + "MaxTranslation";
-        public const string MinRotation = Prefix + "MinRotation";
-        public const string MaxRotation = Prefix + "MaxRotation";
-        public const string ObjectCount = Prefix + "ObjectCount";
+        // object to pick and place
+        public const string ObjectMinDistance = Prefix + "ObjectMinDistance";
+        public const string ObjectMaxDistance = Prefix + "ObjectMaxDistance";
+        public const string ObjectMaxRotationOffset = Prefix + "ObjectMaxRotationOffset";
 
+        // goal for placing
+        public const string GoalMinDistance = Prefix + "GoalMinDistance";
+        public const string GoalMaxDistance = Prefix + "GoalMaxDistance";
+        public const string GoalMaxRotationOffset = Prefix + "GoalMaxRotationOffset";
+
+        // obstacles around object
+        public const string ObstacleMinDistance = Prefix + "ObstacleMinDistance";
+        public const string ObstacleMaxDistance = Prefix + "ObstacleMaxDistance";
+        public const string ObstacleMinCount = Prefix + "ObstacleMinCount";
+        public const string ObstacleMaxCount = Prefix + "ObstacleMaxCount";
+
+        // object types
         public const string UseObjectTypeSquare = Prefix + "UseObjectTypeSquare";
         public const string UseObjectTypeCylinder = Prefix + "useObjectTypeCylinder";
         public const string UseObjectTypeSphere = Prefix + "UseObjectTypeSphere";
 
+        // round
         public const string RoundDuration = Prefix + "RoundDuration";
     }
 }
--- Assets/VRQualityTesting/Scripts/PickAndPlaceMenu/Scribe.cs
+++ Assets/VRQualityTesting/Scripts/PickAndPlaceMenu/Scribe.cs
@@ -9,15 +9,23 @@ namespace VRQualityTesting.Scripts.PickAndPlaceMenu
     public class Scribe : MonoBehaviour
     {
         // Objects tab
-        [SerializeField] private TMP_InputField minTranslationField;
-        [SerializeField] private TMP_InputField maxTranslationField;
-        [SerializeField] private TMP_InputField minRotationField;
-        [SerializeField] private TMP_InputField maxRotationField;
-        [SerializeField] private TMP_InputField objectCountField;
+        [SerializeField] private TMP_InputField objectMinDistanceField;
+        [SerializeField] private TMP_InputField objectMaxDistanceField;
+        [SerializeField] private TMP_InputField objectMaxRotationOffsetField;
         [SerializeField] private Toggle useObjectTypeSquareField;
         [SerializeField] private Toggle useObjectTypeCylinderField;
         [SerializeField] private Toggle useObjectTypeSphereField;
-        // [SerializeField] private TMP_Dropdown weaponTypeDropdown;
+
+        // Obstacles tab
+        [SerializeField] private TMP_InputField obstacleMinDistanceField;
+        [SerializeField] private TMP_InputField obstacleMaxDistanceField;
+        [SerializeField] private TMP_InputField obstacleMinCountField;
+        [SerializeField] private TMP_InputField obstacleMaxCountField;
+
+        // Goals tab
+        [SerializeField] private TMP_InputField goalMinDistanceField;
+        [SerializeField] private TMP_InputField goalMaxDistanceField;
+        [SerializeField] private TMP_InputField goalMaxRotationOffsetField;
 
         // Round tab
         [SerializeField] private TMP_InputField roundDurationField;
@@ -27,21 +35,35 @@ namespace VRQualityTesting.Scripts.PickAndPlaceMenu
         private void InitializeSettings()
         {
             InitializeObjectsSettings();
+            InitializeGoalsSettings();
+            InitializeObstaclesSettings();
             InitializeRoundSettings();
         }
 
         private void InitializeObjectsSettings()
         {
-            minTranslationField.text = Settings.GetFloat(PickAndPlaceKeys.MinTranslation, defaultValue: 0f).ToString(CultureInfo.InvariantCulture);
-            maxTranslationField.text = Settings.GetFloat(PickAndPlaceKeys.MaxTranslation, defaultValue: 0f).ToString(CultureInfo.InvariantCulture);
-            minRotationField.text = Settings.GetFloat(PickAndPlaceKeys.MinRotation, defaultValue: 0f).ToString(CultureInfo.InvariantCulture);
-            maxRotationField.text = Settings.GetFloat(PickAndPlaceKeys.MaxRotation, defaultValue: 0f).ToString(CultureInfo.InvariantCulture);
-
-            objectCountField.text = Settings.GetInt(PickAndPlaceKeys.ObjectCount, defaultValue: 0).ToString(CultureInfo.InvariantCulture);
+            objectMinDistanceField.text = Settings.GetFloat(PickAndPlaceKeys.ObjectMinDistance, defaultValue: 1f).ToString(CultureInfo.InvariantCulture);
+            objectMaxDistanceField.text = Settings.GetFloat(PickAndPlaceKeys.ObjectMaxDistance, defaultValue: 2f).ToString(CultureInfo.InvariantCulture);
+            objectMaxRotationOffsetField.text = Settings.GetFloat(PickAndPlaceKeys.ObjectMaxRotationOffset, defaultValue: 180f).ToString(CultureInfo.InvariantCulture);
 
             useObjectTypeSquareField.isOn = Settings.GetBool(PickAndPlaceKeys.UseObjectTypeSquare, defaultValue: true);
             useObjectTypeCylinderField.isOn = Settings.GetBool(PickAndPlaceKeys.UseObjectTypeCylinder, defaultValue: true);
             useObjectTypeSphereField.isOn = Settings.GetBool(PickAndPlaceKeys.UseObjectTypeSphere, defaultValue: true);
+        }
+
+        private void InitializeGoalsSettings()
+        {
+            goalMinDistanceField.text = Settings.GetFloat(PickAndPlaceKeys.GoalMinDistance, defaultValue: 1f).ToString(CultureInfo.InvariantCulture);
+            goalMaxDistanceField.text = Settings.GetFloat(PickAndPlaceKeys.GoalMaxDistance, defaultValue: 2f).ToString(CultureInfo.InvariantCulture);
+            goalMaxRotationOffsetField.text = Settings.GetFloat(PickAndPlaceKeys.GoalMaxRotationOffset, defaultValue: 180f).ToString(CultureInfo.InvariantCulture);
+        }
+
+        private void InitializeObstaclesSettings()
+        {
+            obstacleMinDistanceField.text = Settings.GetFloat(PickAndPlaceKeys.ObstacleMinDistance, defaultValue: 1f).ToString(CultureInfo.InvariantCulture);
+            obstacleMaxDistanceField.text = Settings.GetFloat(PickAndPlaceKeys.ObstacleMaxDistance, defaultValue: 2f).ToString(CultureInfo.InvariantCulture);
+            obstacleMinCountField.text = Settings.GetInt(PickAndPlaceKeys.ObstacleMinCount, defaultValue: 0).ToString(CultureInfo.InvariantCulture);
+            obstacleMaxCountField.text = Settings.GetInt(PickAndPlaceKeys.ObstacleMaxCount, defaultValue: 0).ToString(CultureInfo.InvariantCulture);
         }
 
         private void InitializeRoundSettings()
@@ -52,21 +74,38 @@ namespace VRQualityTesting.Scripts.PickAndPlaceMenu
         public void SaveSettings()
         {
             SaveObjectsSettings();
+            SaveGoalsSettings();
+            SaveObstaclesSettings();
             SaveRoundSettings();
         }
 
         private void SaveObjectsSettings()
         {
-            Settings.SetFloat(PickAndPlaceKeys.MinTranslation, float.Parse(minTranslationField.text, CultureInfo.InvariantCulture));
-            Settings.SetFloat(PickAndPlaceKeys.MaxTranslation, float.Parse(maxTranslationField.text, CultureInfo.InvariantCulture));
-            Settings.SetFloat(PickAndPlaceKeys.MinRotation, float.Parse(minRotationField.text, CultureInfo.InvariantCulture));
-            Settings.SetFloat(PickAndPlaceKeys.MaxRotation, float.Parse(maxRotationField.text, CultureInfo.InvariantCulture));
+            Settings.SetFloat(PickAndPlaceKeys.ObjectMinDistance, float.Parse(objectMinDistanceField.text, CultureInfo.InvariantCulture));
+            Settings.SetFloat(PickAndPlaceKeys.ObjectMaxDistance, float.Parse(objectMaxDistanceField.text, CultureInfo.InvariantCulture));
 
-            Settings.SetInt(PickAndPlaceKeys.ObjectCount, int.Parse(objectCountField.text, CultureInfo.InvariantCulture));
+            Settings.SetFloat(PickAndPlaceKeys.ObjectMaxRotationOffset, float.Parse(objectMaxRotationOffsetField.text, CultureInfo.InvariantCulture));
 
             Settings.SetBool(PickAndPlaceKeys.UseObjectTypeSquare, useObjectTypeSquareField.isOn);
             Settings.SetBool(PickAndPlaceKeys.UseObjectTypeCylinder, useObjectTypeCylinderField.isOn);
             Settings.SetBool(PickAndPlaceKeys.UseObjectTypeSphere, useObjectTypeSphereField.isOn);
+        }
+
+        private void SaveGoalsSettings()
+        {
+            Settings.SetFloat(PickAndPlaceKeys.GoalMinDistance, float.Parse(goalMinDistanceField.text, CultureInfo.InvariantCulture));
+            Settings.SetFloat(PickAndPlaceKeys.GoalMaxDistance, float.Parse(goalMaxDistanceField.text, CultureInfo.InvariantCulture));
+
+            Settings.SetFloat(PickAndPlaceKeys.GoalMaxRotationOffset, float.Parse(goalMaxRotationOffsetField.text, CultureInfo.InvariantCulture));
+        }
+
+        private void SaveObstaclesSettings()
+        {
+            Settings.SetFloat(PickAndPlaceKeys.ObstacleMinDistance, float.Parse(obstacleMinDistanceField.text, CultureInfo.InvariantCulture));
+            Settings.SetFloat(PickAndPlaceKeys.ObstacleMaxDistance, float.Parse(obstacleMaxDistanceField.text, CultureInfo.InvariantCulture));
+
+            Settings.SetInt(PickAndPlaceKeys.ObstacleMinCount, int.Parse(obstacleMinCountField.text, CultureInfo.InvariantCulture));
+            Settings.SetInt(PickAndPlaceKeys.ObstacleMaxCount, int.Parse(obstacleMaxCountField.text, CultureInfo.InvariantCulture));
         }
 
         private void SaveRoundSettings()
